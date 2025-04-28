b598c3452df270ce1b860608b7ea000b3bf3909f
thibault-cuperlier <thibcup@gmail.com>
2025-04-28 21:26:54 +0200

    Auto-sync commit

--- chifrementasymetric/fini/message/aleatoir.py
+++ chifrementasymetric/fini/message/aleatoir.py
@@ -70,4 +70,5 @@ def generer_nombre_premier_chiffres(chiffres):
 
 # Exemple d'utilisation
 nombre_de_chiffres = 5  # Remplacez par le nombre de chiffres souhaité
-print(generer_nombre_premier_chiffres(nombre_de_chiffres))
+nombre_premier = generer_nombre_premier_chiffres(nombre_de_chiffres)
+print(f"Nombre premier avec {nombre_de_chiffres} chiffres : {nombre_premier}")
